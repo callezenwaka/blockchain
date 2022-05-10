72752b7db5787b2f08e53342ad779cf21981e198
Callis Ezenwaka <callisezenwaka@gmail.com>
2022-05-10 10:27:46 +0100

    Change main script to konfidio

--- Notes.md
+++ Notes.md
@@ -1,1 +1,7 @@
 node konfidio.js init <balances> <transactions> <blockSize>
+
+let konfidio = require('./konfidio');
+
+konfidio.init([100,100,500],[[0,1,50],[1,2,80],[2,0,450]],2);
+
+konfidio.getAccountBalance(0);
--- konfidio.js
+++ konfidio.js
@@ -1,38 +1,55 @@
-// #!/usr/bin/env node
-// const { program } = require('commander');
-const { Command } = require('commander');
-const program = new Command();
-const { init, getAccountBalance, } = require('./index');
+const { verifyTransaction } = require('./utils');
+const { transactions, balances } = require('./database');
 
-program
-  .name('konfidio')
-  .version('1.0.0')
-  // .alias('v')
-  .description('konfidio Blockchain Platform')
+// Initialize Blockchain
+const init = async (_balances, _transactions, _blockSize) => {
+  try {
+		// TODO: initialize blockchain with parameters
+    // const _balances = JSON.parse(bal);
+    // const _transactions = JSON.parse(tx);
+    // const _blockSize = Number(size);
+    if (!_balances || !_transactions || !_blockSize) 
+      return console.info('Initialization failed!');
 
-// program
-//   .help(`
-//     Function                              Alias        Description
-//     konfidio-cli version                    v             To check the version of the konfidio-cli
-//     konfidio-cli init                       i             To initialize blockchain in the database
-//     konfidio-cli getAccountBalance [INDEX]  g             To find a retrieve specific account from the database
-//   `)
+    (await balances).push(..._balances);
+    // items.push(..._balances);
+    const blockSize = _blockSize;
+    for (let i = 0; i < _transactions.length; i++) {
+      let isValid = await verifyTransaction((await balances)[_transactions[i][0]], _transactions[i]);
+      if (!isValid) {
+        continue;
+      }
+      (await balances)[_transactions[i][0]] -= _transactions[i][2];
+      (await balances)[_transactions[i][1]] += _transactions[i][2];
+      (await transactions).push(_transactions[i]);
+      console.info((await balances));
+      // console.info(items);
+    }
+    return console.info('New Customer Added');
+  } catch (error) {
+    console.info(error);
+    return console.info('Internal Server Error!');
+  }
+}
 
-// Initialize Command
-program
-  .command('init <balances> <transactions> <blockSize>')
-  .alias('i')
-  .description('Initialize blockchain')
-  .action(async (balances, transactions, blockSize) => {
-    await init(balances, transactions, blockSize);
-  });
+// Get Account Balance
+const getAccountBalance = async (index) => {
+	try {
+		// Todo: get account balance for a given index
+    console.info(typeof index);
+    console.info((await balances));
+    if (index < 0 || index >= (await balances).length) return console.info('Invalid index!');;
+    const balance = (await balances)[index];
 
+    if (!balance) return console.info(null);
+    else return console.info(balance);
+  } catch (error) {
+		return console.info('Internal Server Error!');
+	}
+}
 
-// Get Balance Command
-program
-  .command('getAccountBalance <index>')
-  .alias('g')
-  .description('Get account balance')
-  .action(async index => await getAccountBalance(index));
-
-program.parse(process.argv);
+// Export All Methods
+module.exports = {
+  init,
+  getAccountBalance,
+}
